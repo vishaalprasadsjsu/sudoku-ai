8345a1ab66b2df5ab1bb25077948e995d311c5b0
Anthony Vo <avyu94@gmail.com>
2017-04-29 23:50:51 -0700

    finished get possibilites method that gets all possibilities within section, row and column

--- SudokuAI.py
+++ SudokuAI.py
@@ -7,20 +7,128 @@ class Sudoku:
     def __init__(self):
         self.sudoku_table = [0]*9
         self.read_table()
-        for i in range(len(self.sudoku_table)):
-            for j in range(len(self.sudoku_table)):
+        for i in range(0,len(self.sudoku_table)):
+            for j in range(0,len(self.sudoku_table)):
                 if self.sudoku_table[i][j] == "0":
                     
                     self.sudoku_table[i][j]+= self.get_possibilities(i,j,self.sudoku_table)
     def get_possibilities(self,i,j,board):
         possibilites = ""
-        for j in range(1,9):
+        for potential_poss in range(1,10):
+            # get first character of each location of section
+            first_character_list = list()
+            for x in range(0,9):
+                first_character_list.append(board[i][x][0])
             # check section
-            if j not in board[i]:
+            if str(potential_poss) not in first_character_list:
                 # check row
-                
+                row = list()
+                if j<3:
+                     if i<3:
+                         for k in range(0,3):
+                            row.append(board[k][0][0])
+                            row.append(board[k][1][0])                
+                            row.append(board[k][2][0])  
+                     elif i>=3 and i <6:
+                         for k in range(3,6):
+                            row.append(board[k][0][0])
+                            row.append(board[k][1][0])                
+                            row.append(board[k][2][0])   
+                     elif i>=6:
+                         for k in range(6,9):
+                            row.append(board[k][0][0])
+                            row.append(board[k][1][0])                
+                            row.append(board[k][2][0])                            
+                            
+                elif j>=3 & j<6:
+                     if i<3:
+                         for k in range(0,3):
+                            row.append(board[k][3][0])
+                            row.append(board[k][4][0])                
+                            row.append(board[k][5][0])                
+                     elif i>=3 and i <6:
+                         for k in range(3,6):
+                            row.append(board[k][3][0])
+                            row.append(board[k][4][0])                
+                            row.append(board[k][5][0])   
+                     elif i>=6:
+                         for k in range(6,9):
+                            row.append(board[k][3][0])
+                            row.append(board[k][4][0])                
+                            row.append(board[k][5][0])                            
+                elif j>=6:
+                     if i<3:
+                         for k in range(0,3):
+                            row.append(board[k][6][0])
+                            row.append(board[k][7][0])                
+                            row.append(board[k][8][0])                
+                     elif i>=3 and i <6:
+                         for k in range(3,6):
+                            row.append(board[k][6][0])
+                            row.append(board[k][7][0])                
+                            row.append(board[k][8][0])   
+                     elif i>=6:
+                         for k in range(6,9):
+                            row.append(board[k][6][0])
+                            row.append(board[k][7][0])                
+                            row.append(board[k][8][0])              
+                if str(potential_poss) not in row:
+                    # check columns
+                    column = list()
+                    if j%3==0:
+                        if i%3 ==0:
+                            for k in range(0,7,3):
+                                column.append(board[k][0][0])
+                                column.append(board[k][3][0])                
+                                column.append(board[k][6][0])  
+                        elif i==1 or i==4 or i==7:
+                            for k in range(1,8,3):
+                                column.append(board[k][0][0])
+                                column.append(board[k][3][0])                
+                                column.append(board[k][6][0])   
+                        elif i==2 or i==5 or i==8:
+                            for k in range(2,9,3):
+                                column.append(board[k][0][0])
+                                column.append(board[k][3][0])                
+                                column.append(board[k][6][0])                            
+                                
+                    elif j==1 or j==4 or j==7:
+                        if i%3 ==0:
+                            for k in range(0,7,3):
+                                column.append(board[k][1][0])
+                                column.append(board[k][4][0])                
+                                column.append(board[k][7][0])  
+                        elif i==1 or i==4 or i==7:
+                            for k in range(1,8,3):
+                                column.append(board[k][1][0])
+                                column.append(board[k][4][0])                
+                                column.append(board[k][7][0])   
+                        elif i==2 or i==5 or i==8:
+                            for k in range(2,9,3):
+                                column.append(board[k][1][0])
+                                column.append(board[k][4][0])                
+                                column.append(board[k][7][0])                             
+                    elif i==2 or i==5 or i==8:
+                        if i%3 ==0:
+                            for k in range(0,7,3):
+                                column.append(board[k][2][0])
+                                column.append(board[k][5][0])                
+                                column.append(board[k][8][0])  
+                        elif i==1 or i==4 or i==7:
+                            for k in range(1,8,3):
+                                column.append(board[k][2][0])
+                                column.append(board[k][5][0])                
+                                column.append(board[k][8][0])   
+                        elif i==2 or i==5 or i==8:
+                            for k in range(2,9,3):
+                                column.append(board[k][2][0])
+                                column.append(board[k][5][0])                
+                                column.append(board[k][8][0])             
+                    if str(potential_poss) not in column:
+                        possibilites+=str(potential_poss)
+                  
                     
-                possibilites+=str(j)
+                
                 
         
         return possibilites
@@ -61,6 +169,39 @@ class Sudoku:
         
         
     # Prints Sudoku in a nice, standard format
+    def print_table_with_possibilities(self):
+            for sections_of_three in range(3):
+           	    for row in range(3):
+          		for i in range(0+sections_of_three*3,3*(sections_of_three+1)):
+         			if i % 3 ==0 :
+            				print ""
+
+            				for z in range(0+(row*3),3*(row+1)):
+           					#print "\nrow is %s " % row
+           					#print "\ntimes run %s"  % z
+           					#print "\n i is %s" % i
+           					if self.sudoku_table[i][z][1:] != "":
+           					    print "(%s: poss %9s)" % (self.sudoku_table[i][z][0], self.sudoku_table[i][z][1:]),
+           					else:
+                                                    print "(%s: %14s)" % (self.sudoku_table[i][z][0], ""),
+            				print "            ",
+         			else:
+            				for z in range(0+(row*3),3*(row+1)):
+           					#print "\nrow is %s " % row
+           					#print "\ntimes run %s"  % z
+           					#print "\n i is %s" % i
+           					if self.sudoku_table[i][z][1:] != "":
+           					    print "(%s: poss %9s)" % (self.sudoku_table[i][z][0], self.sudoku_table[i][z][1:]),
+           					else:
+                                                    print "(%s: %14s)" % (self.sudoku_table[i][z][0], ""),
+            				print "            ",
+           	    print ""
+           	    print ""
+           	    print ""
+            
+
+
+
     def print_table(self):
             for sections_of_three in range(3):
            	    for row in range(3):
@@ -71,29 +212,42 @@ class Sudoku:
            					#print "\nrow is %s " % row
            					#print "\ntimes run %s"  % z
            					#print "\n i is %s" % i
-           					    print "%s" % self.sudoku_table[i][z][0],
-            
+           					
+           					print "%s" % self.sudoku_table[i][z][0],
+           					
             				print " ",
          			else:
             				for z in range(0+(row*3),3*(row+1)):
            					#print "\nrow is %s " % row
            					#print "\ntimes run %s"  % z
            					#print "\n i is %s" % i
-           					    print "%s" % self.sudoku_table[i][z][0],
+           					
+           					print "%s" % self.sudoku_table[i][z][0],
+           					
             				print " ",
            	    print ""
             
-
-
-
-
-    def solve(board):
-       
+    # TO BE DONE
+    def get_cells_with_allowed_num_poss(self):
         return
+
+    # TO BE DONE
+    def board_filled(self):
+        return
+        
+    def solve(self,board):
+        if self.board_filled():
+            return True
+        
+        for i in range(1,10):
+            allowed_possibilities = self.get_cells_with_allowed_num_poss()
+        #REST OF CODE TO BE DONE
+
 
 """
 Test
 """
 s = Sudoku()
-print s.sudoku_table[0][1]
+print s.sudoku_table[0]
 print s.print_table()
+print s.print_table_with_possibilities()
