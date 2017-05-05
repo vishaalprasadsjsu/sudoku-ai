15a759dbf0701080e5b3ad732ef27624d1ec46be
Stephen <sing.stephen95@gmail.com>
2017-05-04 17:40:22 -0700

    Added board filled method

--- SudokuAI.py
+++ SudokuAI.py
@@ -232,9 +232,13 @@ class Sudoku:
 
         return
 
-    # TO BE DONE
+    #Returns a boolean depending upon if each cell has a single possibility
     def board_filled(self):
-        return
+        for i in range(9):
+            for j in range(9):
+                if len(self.sudoku_table[i][j]) > 1:
+                    return False
+        return True
         
     # TO BE DONE
     def place_cell_in_board(self,section, position, board):
@@ -271,3 +275,4 @@ Test
 """
 s = Sudoku()
 print s.sudoku_table[0]
+print s.board_filled()
